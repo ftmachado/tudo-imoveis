2e1d0d84e1904d08b575b65c6abd64dd5b8cdbdc
Fhabiana Machado <thielisantos@hotmail.com>
2019-09-21 23:32:17 -0300

    LoginFormAuthenticator

--- src/Entity/Pessoa.php
+++ src/Entity/Pessoa.php
@@ -3,12 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Table(name="pessoa")
  * @ORM\Entity(repositoryClass="App\Repository\PessoaRepository")
  */
-class Pessoa
+class Pessoa implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -16,6 +17,22 @@ class Pessoa
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="string", length=11)
@@ -33,11 +50,6 @@ class Pessoa
     private $nome;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $email;
-
-    /**
      * @ORM\Column(name="telefone_principal", type="string", length=15, nullable=true)
      */
     private $telefonePrincipal;
@@ -51,11 +63,6 @@ class Pessoa
      * @ORM\Column(type="string", length=45, nullable=true)
      */
     private $usuario;
-
-    /**
-     * @ORM\Column(type="string", length=45, nullable=true)
-     */
-    private $senha;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Estado")
@@ -110,6 +117,79 @@ class Pessoa
         return $this->id;
     }
 
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
     public function getCpf(): ?string
     {
         return $this->cpf;
@@ -146,18 +226,6 @@ class Pessoa
         return $this;
     }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(?string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
     public function getTelefonePrincipal(): ?string
     {
         return $this->telefonePrincipal;
@@ -190,18 +258,6 @@ class Pessoa
     public function setUsuario(?string $usuario): self
     {
         $this->usuario = $usuario;
-
-        return $this;
-    }
-
-    public function getSenha(): ?string
-    {
-        return $this->senha;
-    }
-
-    public function setSenha(?string $senha): self
-    {
-        $this->senha = $senha;
 
         return $this;
     }
