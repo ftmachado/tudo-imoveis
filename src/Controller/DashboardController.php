6393e61f76133434c8dac5f5846da24da5bd1718
Fhabiana Machado <thielisantos@hotmail.com>
2019-09-26 02:17:30 -0300

    Ajustes da dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Imobiliaria;
+use App\Entity\Imovel;
+use App\Entity\Pessoa;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +15,23 @@ class DashboardController extends AbstractController
      */
     public function index()
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $imob = $em->getRepository(Imobiliaria::class)->findOneById(1);
+
+        $total = [];
+        $vender = $em->getRepository(Imovel::class)->findBy(['tipoAnuncio' => 'vender', 'status' => 'disponivel']);
+        $alugar = $em->getRepository(Imovel::class)->findBy(['tipoAnuncio' => 'alugar', 'status' => 'disponivel']);
+        $cliente = $em->getRepository(Pessoa::class)->findBy(['cliente' => true]);
+
+        $total['vender'] = count($vender);
+        $total['alugar'] = count($alugar);
+        $total['cliente'] = count($cliente);
+
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'nomeImobiliaria' => $imob->getNomeFantasia(),
+            'total' => $total
         ]);
     }
 }
