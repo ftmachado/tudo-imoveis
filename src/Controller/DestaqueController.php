67ca3041e22629a365cf804055cf6cb7113715a7
Fhabiana Machado <thielisantos@hotmail.com>
2019-09-26 02:36:18 -0300

    Telas restantes aviso e login

---
+++ src/Controller/DestaqueController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DestaqueController extends AbstractController
+{
+    /**
+     * @Route("/destaque", name="destaque_index")
+     */
+    public function index()
+    {
+        return $this->render('destaque/index.html.twig', [
+        ]);
+    }
+}
